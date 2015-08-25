b098bd3313ecd3b5796e5f817e2aa328c4a90d06
viktor <viktor@niviol.com>
2015-08-25 09:11:08 -0400

    First test

---
+++ src/model/Entry.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Entry {
+	public String name;
+	public String userName;
+	public String password;
+	public String url;
+	public String tag;
+	public String comment;	
+}
---
+++ src/model/User.java
@@ -0,0 +1,6 @@
+package model;
+
+public class User {
+	public String login;
+	public String password;
+}
